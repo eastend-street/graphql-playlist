3975e6abeb5c4bdfa0b9f3f445b187312b2403a1
eastend-street <suminari.yamada@gmail.com>
2023-04-05 23:36:53 +0900

    feat: connect to mongodb

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log('connected to database');
+});
 
 const app = express();
 
