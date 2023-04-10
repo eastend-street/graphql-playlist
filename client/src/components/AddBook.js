7325787e49beaaf00bc57f2754936e16cc103a03
eastend-street <suminari.yamada@gmail.com>
2023-04-10 23:34:08 +0900

    feat: implement add a book form (Mutation request has not been implemented yet)

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,32 +1,51 @@
-import { gql, useQuery } from '@apollo/client';
-
-const getAuthorsQuery = gql`
-  {
-    authors {
-      name
-      id
-    }
-  }
-`;
+import { useState } from 'react';
+import { useQuery } from '@apollo/client';
+import { getAuthorsQuery } from '../queries/queries';
 
 function AddBook() {
+  const [name, setName] = useState('');
+  const [genre, setGenre] = useState('');
+  const [authorId, setAuthorId] = useState('');
   const { loading, error, data } = useQuery(getAuthorsQuery);
 
   if (error) return <p>Error: {error.message}</p>;
 
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log({
+      name,
+      genre,
+      authorId,
+    });
+  };
+
   return (
-    <form id="add-book">
+    <form id="add-book" onSubmit={submitForm}>
       <div className="field">
         <label htmlFor="book-name">Book name: </label>
-        <input id="book-name" type="text" />
+        <input
+          id="book-name"
+          type="text"
+          onChange={(event) => setName(event.currentTarget.value)}
+          value={name}
+        />
       </div>
       <div className="field">
         <label htmlFor="genre">Genre: </label>
-        <input id="genre" type="text" />
+        <input
+          id="genre"
+          type="text"
+          onChange={(event) => setGenre(event.currentTarget.value)}
+          value={genre}
+        />
       </div>
       <div className="field">
         <label htmlFor="author">Author: </label>
-        <select name="author" id="author">
+        <select
+          name="author"
+          id="author"
+          onChange={(event) => setAuthorId(event.currentTarget.value)}
+        >
           {loading ? (
             <option>Loading Authors...</option>
           ) : (
@@ -40,6 +59,7 @@ function AddBook() {
             </>
           )}
         </select>
+        <button type="submit">Submit</button>
       </div>
     </form>
   );
